0c2653dc1a1f7127e6ecddf8583675df0cba628f
Maria-Mendez-lol <mendezmariale14@gmail.com>
2025-11-11 11:15:00 -0400

    2ndo Commit (prueba)

---
+++ Ingredientes.py
@@ -0,0 +1,30 @@
+class Ingredientes():
+
+    ''' Representa lo que conforma un HotDog, van a ser gestionados en el inventario.
+    '''
+
+    def __init__(self, nombre : str, tipo : str, cantidad : int):
+        
+        self.nombre = nombre
+        self.tipo = tipo
+        self.cantidad = cantidad
+
+    def agregar():
+        ''' Este metodo permitira al cliente anadir un ingrediente a su HotDog
+        '''
+        pass
+
+    def eliminar():
+        ''' Este metodo permitira al cliente eliminar un ingrediente a su HotDog
+        '''
+        pass
+
+    def listar():
+        ''' Este metodo se encargara de en listar todos los ingredientes por su categoria y luego por su categoria y tipo
+        '''
+        pass
+
+    def actualizar_cantidad():
+        ''' Este metodo permite que se actualice la cantidad de ingredientes que quedan 
+        '''
+        pass
